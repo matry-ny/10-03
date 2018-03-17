8a3ec63ae68800f9e0cfd290fd0e399b3e2dabe7
Dmytro Kotenko <d.kotenko@i.ua>
2018-03-17 10:58:49 +0200

    Added tags and comments demonstration

---
+++ samples/basics/tags.php
@@ -0,0 +1,22 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Test comment
+# Comment string
+/* Test */
+/*
+ * Test
+ * QWerty
+ * dasds
+ */
+/**
+ * @var int $test
+ * @deprecated $test
+ */
+
+var_dump(123, $test);
+
+?>
+
+<h1><?= 'Test' ?></h1>
