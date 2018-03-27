14ad5236537ca2b4b9412ed3a2d2ee814ce4a0e0
Dmytro Kotenko <d.kotenko@i.ua>
2018-03-27 22:58:46 +0300

    Fixed simple numbers example

--- samples/basics/practic/loops/simple-numbers.php
+++ samples/basics/practic/loops/simple-numbers.php
@@ -16,26 +16,75 @@ while (true) {
 
     for (; $start <= $end; $start++)
     {
-        if ($start == 2) {
-            $numbers[] = 2;
+        //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+        if($start == 1) {
             continue;
         }
 
-        if($start == 1 || $start % 2 == 0) {
+        //2 is prime (the only even number that is prime)
+        if($start == 2) {
             continue;
         }
 
-        $tmp = [];
-        for ($i = 1; $i <= $start; $i = $i + 1) {
-            if ($start % $i == 0) {
-                $tmp[] = $i;
+        /**
+         * if the number is divisible by two, then it's not prime and it's no longer
+         * needed to check other even numbers
+         */
+        if($start % 2 == 0) {
+            continue;
+        }
+
+        /**
+         * Checks the odd numbers. If any of them is a factor, then it returns false.
+         * The sqrt can be an aproximation, hence just for the sake of
+         * security, one rounds it to the next highest integer value.
+         */
+        $ceil = ceil(sqrt($start));
+        $continue = false;
+        for($i = 3; $i <= $ceil; $i = $i + 2) {
+            if($start % $i == 0) {
+                $continue = true;
+                break;
             }
         }
 
-        if (count($tmp) == 1) {
-            $numbers[] = $start;
+        if ($continue) {
+            continue;
         }
+
+        $numbers[] = $start;
     }
 
     echo implode(', ', $numbers), PHP_EOL;
 };
+
+function isPrime($num) {
+    //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+    if($num == 1)
+        return false;
+
+    //2 is prime (the only even number that is prime)
+    if($num == 2)
+        return true;
+
+    /**
+     * if the number is divisible by two, then it's not prime and it's no longer
+     * needed to check other even numbers
+     */
+    if($num % 2 == 0) {
+        return false;
+    }
+
+    /**
+     * Checks the odd numbers. If any of them is a factor, then it returns false.
+     * The sqrt can be an aproximation, hence just for the sake of
+     * security, one rounds it to the next highest integer value.
+     */
+    $ceil = ceil(sqrt($num));
+    for($i = 3; $i <= $ceil; $i = $i + 2) {
+        if($num % $i == 0)
+            return false;
+    }
+
+    return true;
+}
